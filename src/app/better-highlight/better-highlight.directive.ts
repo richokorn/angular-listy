4630d9d64a71b5cabc48adb22fc7dab1285ba3ae
Richard Korn <richo.korn@gmail.com>
2022-07-19 10:28:12 +0200

    Add a HostListener mouseenter/leave for styling

--- src/app/better-highlight/better-highlight.directive.ts
+++ src/app/better-highlight/better-highlight.directive.ts
@@ -1,4 +1,10 @@
-import { Directive, ElementRef, OnInit, Renderer2 } from '@angular/core';
+import {
+  Directive,
+  ElementRef,
+  HostListener,
+  OnInit,
+  Renderer2,
+} from '@angular/core';
 
 @Directive({
   selector: '[appBetterHighlight]',
@@ -7,6 +13,22 @@ export class BetterHighlightDirective implements OnInit {
   constructor(private elRef: ElementRef, private renderer: Renderer2) {}
 
   ngOnInit() {
+    this.renderer.setStyle(
+      this.elRef.nativeElement,
+      'background-color',
+      'cyan'
+    );
+  }
+
+  @HostListener('mouseenter') mouseover(eventData: Event) {
+    this.renderer.setStyle(
+      this.elRef.nativeElement,
+      'background-color',
+      '#89CFF0'
+    );
+  }
+
+  @HostListener('mouseleave') mouseleave(eventData: Event) {
     this.renderer.setStyle(
       this.elRef.nativeElement,
       'background-color',
